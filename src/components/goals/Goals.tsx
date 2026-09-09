a89cb6054f23c9e5455568f8ccb795b7990b7437
bcioruvy <si709641@gmail.com>
2026-09-09 13:05:27 +1000

    Adjust grid layout for goals display

--- src/components/goals/Goals.tsx
+++ src/components/goals/Goals.tsx
@@ -115,7 +115,7 @@ export const Goals: React.FC = () => {
           />
         </div>
       ) : (
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
         {goals.map(gl => {
           const progressPercent = Math.min(100, Math.round((gl.currentAmount / gl.targetAmount) * 100));
           const remainingAmount = gl.targetAmount - gl.currentAmount;
